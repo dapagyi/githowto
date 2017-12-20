47ab1b03e9fdb9af02b329c28bc0d5538d2ee1d4
Apagyi Dávid <apagyi.david@gmail.com>
2017-12-20 15:10:28 +0100

    ispal

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 function isPalindrom(x){
     var jegyek = (x.toString()).split('');
-    var isPal=true;
     for (var j=0; j< jegyek.length; j++){
         if(jegyek[j] != jegyek[jegyek.length-j-1]){
-            isPal=false;
+            return false;
         }
     }
-    return isPal;
+    return true;
 }
