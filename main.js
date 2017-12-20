f34c68c31a8aede05dd21ac2b3fa6a3db308556d
Apagyi Dávid <apagyi.david@gmail.com>
2017-12-20 15:05:40 +0100

    isPalindrom();

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-var text = '';
-for (var i=1; i<=1000; i++){
-    if (i%4==0) text += i+', ';
-}
-
 function isPalindrom(x){
     var jegyek = (x.toString()).split('');
-    //console.log(jegyek.length);
     var isPal=true;
     for (var j=0; j< jegyek.length; j++){
-        //console.log(jegyek[j]+'?'+jegyek[jegyek.length-j-1]);
         if(jegyek[j] != jegyek[jegyek.length-j-1]){
             isPal=false;
         }
     }
     return isPal;
 }
-
-console.log(isPalindrom(66566));
